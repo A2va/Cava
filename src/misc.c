2d65d01f445d70fc42027d813dec2abee0f09791
A2va <49582555+A2va@users.noreply.github.com>
2020-07-22 15:27:07 +0200

    Update misc.c

--- src/misc.c
+++ src/misc.c
@@ -9,11 +9,11 @@
 * DESCRIPTION																
 * 	  Misc								                        			
 ****************************************************************************/
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 #include "misc.h"
-
 /****************************************************************************
 * Function IntegerToStr()																																	
 *		Input Parameter: Number								            	
@@ -25,29 +25,35 @@
 ****************************************************************************/
 unsigned char *IntegerToStr(int Number)
 {
-    unsigned int i=0;
-    int NumBack = 0;
-    NumBack = Number;
-    unsigned char* String = malloc(sizeof(*String));//String Declaration
-    if(String==NULL)
+    int NumBack = Number;
+    unsigned int i = 0;
+    do
     {
-        exit(EXIT_FAILURE);
+        i++;
+        NumBack /= 10;
+    } while (NumBack != 0);
+    unsigned char* String = malloc((sizeof(unsigned char)*i)+1);//String Declaration
+    if (String == NULL)
+    {
+        return 0;
     }
+    i = 0;
+    NumBack = Number;
     Number = fabs(Number);
+
     while(Number !=0)
     {
         String[i]=(Number %10)+48;
         Number = Number /10;
-        String = realloc(String,sizeof(*String)*(i+2));//Realloc a frame
-        if (String == NULL)
-        {
-            exit(EXIT_FAILURE);
-        }
         i++;
     }
     if (NumBack < 0)
     {
-        String = realloc(String, sizeof(*String) * (i + 2));//Realloc a frame
+        String = realloc(String, sizeof(unsigned char) * i + 2);//Realloc a frame
+        if(String == NULL)
+        {
+            return 0;
+        }
         String[i] = '-';
         String[i + 1] = '\0';
     }
@@ -72,31 +78,41 @@ unsigned char* DoubleToStr(double Number,unsigned int NumberDigit)
 {
     unsigned int i = 0;
     int NumberRel = Number;
-    unsigned char* String1 = malloc(sizeof(*String1));//String Declaration
-    String1 = IntegerToStr(Number);
-    unsigned char* String2 = malloc(sizeof(*String2));//String Declaration
-    if (String2==NULL)
+    unsigned char* String1 = IntegerToStr((int)Number);
+    NumberRel = (fabs(Number) - fabs(NumberRel)) * pow(10, NumberDigit);
+    int NumBack = NumberRel;
+    do
     {
-       exit(EXIT_FAILURE);
+        i++;
+        NumBack /= 10;
+    } while (NumBack != 0);
+
+    unsigned char* String2 = malloc((sizeof(unsigned char)*i)+1);//String Declaration
+    if(String2==NULL)
+    {
+       return 0;
     }
-    NumberRel = (fabs(Number) - fabs(NumberRel)) * pow(10,NumberDigit);
+    i=0;
     while (NumberRel != 0)
     {
         String2[i] = (NumberRel % 10) + 48;
         NumberRel = NumberRel / 10;
-        String2 = realloc(String2, sizeof(*String2) * (i + 2));//Realloc a frame
-        if (String2 == NULL)
-        {
-            exit(EXIT_FAILURE);
-        }
         i++;
     }
     String2 = realloc(String2, sizeof(*String2) * (i + 2));//Realloc a frame
+    if(String2==NULL)
+    {
+       return 0;
+    }
     String2[i] = '.';
     String2[i + 1] = '\0';
    
     str_Reverse(String2);
     String1 = realloc(String1, sizeof(*String1) * (strlen(String1) + i+2));
+    if(String1==NULL)
+    {
+       return 0;
+    }
     strcat(String1, String2);
     return String1;
 }
